ec2920fc990349977b90045da82e17c460316d10
dannypethuru <dannycse083@gmail.com>
2023-07-03 10:35:08 +0530

    method overlaoding

---
+++ Overload.java
@@ -0,0 +1,23 @@
+package practice.learnning;
+
+public class Overload {
+
+	
+	public int sum(int x, int y)
+	{
+//		int sum;
+//		sum=x+y;
+//		System.out.println(sum);
+		return (x+y);
+		
+	}
+	public int sum(int x,int y,int z)
+	{
+		return (x+y+z);
+		
+	}
+	public double sum(double a,double b)
+	{
+		return (a+b);
+	}
+}
---
+++ Overload2.java
@@ -0,0 +1,17 @@
+package practice.learnning;
+
+public class Overload2 extends Overload{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Overload2 ov = new Overload2();
+//ov.sum(13,12);
+//ov.sum(21,2, 34);
+//ov.sum(23, 34);
+System.out.println(ov.sum(13,12));
+System.out.println(ov.sum(16,05,9));
+System.out.println(ov.sum(13.5,12.4));
+
+	}
+
+}
